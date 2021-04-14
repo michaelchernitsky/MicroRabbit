216d5ca41305da872cdc1b655aeaf9b4336d8756
Michael Chernitsky <michaelchernitsky@gmail.com>
2021-04-14 12:33:35 +0300

    banking service added

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IMediator, Mediator>();
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            //ApplicationServices
+            services.AddTransient<IAccountService, AccountService>();
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
+
+
+        } 
+    }
+}
